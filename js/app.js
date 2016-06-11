f1ccc4c887e174a955ddd84082f6f2f7dc73fa66
llanginger <llanginger@gmail.com>
2016-06-10 18:07:32 -0700

    Fixed logo hiding

--- js/app.js
+++ js/app.js
@@ -36,8 +36,8 @@ $(function() {
       '<div class="col-md-6 top-30">' +
         '<div class="hovereffect center-block">' +
           '<img class="img-responsive portfolio-pic" src="' + webPortfolioItems[item].imgSrc + '" alt="">' +
-            '<div class="overlay">' +
-              '<h2>' + webPortfolioItems[item].title + '</h2>' +
+          '<div class="overlay">' +
+            '<h2>' + webPortfolioItems[item].title + '</h2>' +
             '<p>' +
               '<a target="_blank" href="' + webPortfolioItems[item].linkSrc + '">LINK HERE</a>' +
             '</p>' +
@@ -52,8 +52,8 @@ $(function() {
       '<div class="col-md-6 top-30">' +
         '<div class="hovereffect center-block">' +
           '<img class="img-responsive portfolio-pic" src="' + musicPortfolioItems[item].imgSrc + '" alt="">' +
-            '<div class="overlay">' +
-              '<h2>' + musicPortfolioItems[item].title + '</h2>' +
+          '<div class="overlay">' +
+            '<h2>' + musicPortfolioItems[item].title + '</h2>' +
             '<p>' +
               '<a target="_blank" href="' + musicPortfolioItems[item].linkSrc + '">LINK HERE</a>' +
             '</p>' +
@@ -68,9 +68,20 @@ $(function() {
 
     console.log(Math.round(wScroll));
 
+    $(".logo-box").css({
+      "transform": "translate(0px, " + wScroll /3.75 + "%)"
+    })
+
+    // TODO fix this
+    if (wScroll > 275) {
+      $(".logo-box").fadeOut();
+    } else {
+      $(".logo-box").fadeIn();
+    };
+
   })
 
-  $(".suffix").mouseenter(function() {
+  $(".suffix, .check-out").mouseenter(function() {
     $(this).css("color", "red");
   }).mouseleave(function() {
     $(this).css("color", "black");
@@ -84,9 +95,10 @@ $(function() {
 
   $(".down-icon").click(function(e){
     $("html, body").animate({
-      scrollTop: $("#aboutMe").offset().top
+      scrollTop: $("#my-work").offset().top
     }, 1000)
   })
+
 
 
 
